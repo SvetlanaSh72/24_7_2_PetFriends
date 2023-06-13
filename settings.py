ccc5ce22926c4913410e7b117adf5c5665034d12
SvetlanaSh72 <95474551+SvetlanaSh72@users.noreply.github.com>
2023-06-13 11:24:09 +0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,8 @@
+
+valid_email = "sh_svetochka@internet.ru"
+valid_password = "firefox_sv"
+
+not_valid_email = "sh1_svetochka@internet.ru"
+not_valid_password = "firefox_sv1"
+
+uncorrect_auth_key = "ac8f9747c10e6d516a54addc910fb3eb98475362b05c3bb6b3f4bebe1"
